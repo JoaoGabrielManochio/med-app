57536534715026eb6c695e4db810cb5271d8b1bf
João Gabriel Silva <joao.silva@irroba.com.br>
2021-08-31 23:40:35 -0300

    start dashboard

---
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\UpdateUserRequest;
+use App\Http\Requests\Dashboard\StoreUserRequest;
+use App\Services\UserService;
+use App\DataTables\UsersDataTable;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(UsersDataTable $dataTable)
+    {
+        return $dataTable->render('pages.users.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $user = $this->userService->store();
+
+        if (!$user) {
+
+        } else {
+
+        }
+
+        return redirect()->route('users.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = $this->userService->show($id);
+
+        if (!$user) {
+            return redirect()->route('users.index');
+        }
+
+        return view('pages.users.edit')->with(compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $user = $this->userService->update($id);
+
+        if (!$user) {
+
+        } else {
+
+        }
+
+        return redirect()->route('users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = $this->userService->destroy($id);
+
+        if (!$user) {
+
+        } else {
+
+        }
+
+        return redirect()->route('users.index');
+    }
+}
