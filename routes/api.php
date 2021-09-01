2e64e9657cb0682b1141886fff27f1beba0c79a3
João Gabriel Silva <joao.silva@irroba.com.br>
2021-09-01 19:36:23 -0300

    create ping route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix' => 'v1'], function () {
 
+    Route::get('/ping', function () {
+        return response()->json(['now' => date('Ymd H:i:s')]);
+    });
+
     Route::post('token', 'API\UserController@getToken');
 
     Route::group(['middleware' => ['auth:api']], function () {
